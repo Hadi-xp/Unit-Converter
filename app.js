cb523b18bea971631dca29967354530714e74730
Hadixp <hadi.b.2002@gmail.com>
2025-01-14 18:52:33 +0330

    Feat: Km to meters added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//DOM
+//DOM Kg to grams
 const gramsBtn = document.querySelector(".gramsButton");
 const gramsResult = document.querySelector(".grams");
 const gramsError = {
@@ -35,6 +35,41 @@ if (isNaN(Kg)) {
 // taking first Number as Kg
 gramsBtn.addEventListener('click',KgToGrams);
 
+// DOM Km to meters
+const metersBtn = document.querySelector(".metersButton");
+const metersResult = document.querySelector(".meters");
+const metersError = {
+    validError: new Error("Please enter a valid number"),
+    zeroError: new Error("Input cant be zero"),
+    negativeError: new Error("Input cant be negative")
+};
+
+// Km => Meters
+function KmToMeters(){
+    let Km = Number(prompt("Enter the value in Km: "));
+    // validation
+if (isNaN(Km)) {
+    metersResult.textContent = metersError.validError.message;
+    throw metersError.validError;
+    
+} else if(Km === 0) {
+    metersResult.textContent = metersError.zeroError.message;
+    throw metersError.zeroError;
+    
+} else if (Km < 0){
+    metersResult.textContent = metersError.negativeError.message;
+    throw metersError.negativeError;
+}else{
+    // converting it to meters
+    let meters = Km * 1000;
+
+    // displaying the result
+    metersResult.textContent = `${Km} Km = ${meters} meters`;
+}
+}
+
+// taking first Number as Km
+metersBtn.addEventListener('click',KmToMeters);
 
 
 
